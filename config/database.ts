abd18ba9c2e5ab43b346cc31defaa4e0ce165922
joaocarrato <joaocarrato18@gmail.com>
2025-02-27 21:51:38 -0300

    fix

--- config/database.ts
+++ config/database.ts
@@ -1,51 +1,44 @@
-/**
- * Config source: https://git.io/JesV9
- *
- * Feel free to let us know via PR, if you find something broken in this config
- * file.
- */
-
 import Env from '@ioc:Adonis/Core/Env'
 import { DatabaseConfig } from '@ioc:Adonis/Lucid/Database'
+import { parse } from 'pg-connection-string'
+
+// Função para obter configuração a partir da DATABASE_URL
+const getConnectionFromUrl = (): Partial<DatabaseConfig['connections']['pg']> => {
+  if (!Env.get('DATABASE_URL')) {
+    return {}
+  }
+
+  const parsedUrl = parse(Env.get('DATABASE_URL'))
+  
+  return {
+    host: parsedUrl.host || Env.get('PG_HOST'),
+    port: parsedUrl.port ? parseInt(parsedUrl.port) : Env.get('PG_PORT'),
+    user: parsedUrl.user || Env.get('PG_USER'),
+    password: parsedUrl.password || Env.get('PG_PASSWORD'),
+    database: parsedUrl.database || Env.get('PG_DB_NAME'),
+    ssl: { rejectUnauthorized: false }
+  } as Partial<DatabaseConfig['connections']['pg']>
+}
 
 const databaseConfig: DatabaseConfig = {
-  /*
-  |--------------------------------------------------------------------------
-  | Connection
-  |--------------------------------------------------------------------------
-  |
-  | The primary connection for making database queries across the application
-  | You can use any key from the `connections` object defined in this same
-  | file.
-  |
-  */
   connection: Env.get('DB_CONNECTION'),
 
   connections: {
-    /*
-    |--------------------------------------------------------------------------
-    | PostgreSQL config
-    |--------------------------------------------------------------------------
-    |
-    | Configuration for PostgreSQL database. Make sure to install the driver
-    | from npm when using this connection
-    |
-    | npm i pg
-    |
-    */
     pg: {
       client: 'pg',
       connection: {
+        ...getConnectionFromUrl(),
         host: Env.get('PG_HOST', 'localhost'),
         port: Env.get('PG_PORT', 5432),
-        user: Env.get('PG_USER', 'postgres'),
-        password: Env.get('PG_PASSWORD', 'postgres'),
-        database: Env.get('PG_DB_NAME', 'postgres'),
+        user: Env.get('PG_USER', 'nubble'),
+        password: Env.get('PG_PASSWORD', 'nubble'),
+        database: Env.get('PG_DB_NAME', 'nubble_db_development'),
+        ssl: Env.get('NODE_ENV') === 'production' ? { rejectUnauthorized: false } : false,
       },
       migrations: {
         naturalSort: true,
       },
-      healthCheck: true,
+      healthCheck: false,
       debug: false,
     },
   },
